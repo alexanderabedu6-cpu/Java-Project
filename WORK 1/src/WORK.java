59e8ae8fe7f409cea9c2c3648bd5d7d755bba58c
K_Khent <alexanderabedu6@gmail.com>
2026-06-11 03:32:53 +0000

    Add files via upload

---
+++ WORK 1/src/WORK.java
@@ -0,0 +1,6 @@
+public class WORK {
+    public static void main(String[] args) {
+
+        System.out.print("Hello world");
+    }
+}
---
+++ WORK 2/src/WORK.java
@@ -0,0 +1,35 @@
+public class WORK {
+    public static void main(String[] args){
+
+
+        System.out.println(" x                                                                           x  ");
+        System.out.println(" x                                                                           x  ");
+        System.out.println(" x                                                                           x  ");
+        System.out.println(" x                                                                           x  ");
+        System.out.println(" xxxxxxxxxxxxxxx              xxxxxxxxxxxxxxxxxxxxxxx          xxxxxxxxxxxxxxx  ");
+        System.out.println(" x             x              x                     x          x             x  ");
+        System.out.println(" x             x              x                     x          x             x  ");
+        System.out.println(" x             x              x                     x          x             x  ");
+        System.out.println(" x             x              x                     x          x             x  ");
+
+
+
+
+        System.out.println("        TTTT           ");
+        System.out.println("       TTTTTT          ");
+        System.out.println("      TTTTTTTT         ");
+        System.out.println("     TTTTTTTTTT        ");
+        System.out.println("    TTTTTTTTTTTT       ");
+        System.out.println("   TTTTTTTTTTTTTT      ");
+        System.out.println("  TTTTTTTTTTTTTTTT     ");
+        System.out.println(" TTTTTTTTTTTTTTTTTT    ");
+
+
+
+
+
+    }
+
+
+
+}
